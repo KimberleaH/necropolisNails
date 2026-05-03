8ca69878313c70043145370b8066597c6f5b76d8
Kimberlea Heili <62309455+KimberleaH@user.noreply.github.com>
2026-05-03 17:33:38 -0500

    Remove test, move JS files

--- functions/test.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-    const { env } = context;
-    return new Response(JSON.stringify({
-        client_id: env.GITHUB_CLIENT_ID ? "✓ Set" : "✗ Undefined",
-        client_secret: env.GITHUB_CLIENT_SECRET ? "✓ Set" : "✗ Undefined",
-    }), {
-        headers: { 'content-type': 'application/json' }
-    });
-}
